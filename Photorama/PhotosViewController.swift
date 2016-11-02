b5be6d02daa6c01c59d8b298d0a5feb2f920baaf
Marc <marc.oneill2@gmail.com>
2016-11-02 18:51:45 +1100

    Moving PhotosViewController

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Marc O'Neill on 02/11/2016.
+//  Copyright © 2016 marcondev. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
